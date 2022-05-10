ad4a4b3fb028d3a2f2d4a35911e72be62fbb2661
Logan Dracos <logandracos@Logans-MacBook.local>
2022-05-10 09:02:16 -0400

    kelfewklm

--- Pickup_Comps_Inner/React/my-app/src/index.js
+++ Pickup_Comps_Inner/React/my-app/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Form from './Sliders.js'
+import Form from './Sliders.js';
+import loadNames from './calculation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
