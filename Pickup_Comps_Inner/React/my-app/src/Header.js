9c606d74da37bef0e01820406e8033173ed67a5b
Logan Dracos <logandracos@Logans-MacBook.local>
2022-05-12 08:53:24 -0400

    Output and other Front End Things

--- Pickup_Comps_Inner/React/my-app/src/Header.js
+++ Pickup_Comps_Inner/React/my-app/src/Header.js
@@ -5,8 +5,10 @@ class Header extends React.Component {
     render() {
       return(
         <div>
-            <h2> Pickup Basketball Comps</h2>
+            <h2 class="wrap"> Pickup Basketball Comps</h2>
+
             <br />
+
             <p>
                 Insert Instructions Here
             </p>
