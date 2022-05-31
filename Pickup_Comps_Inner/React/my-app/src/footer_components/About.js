88ec455a0659e58f0f95a6c3d8fd0682c389c16b
Logan Dracos <logandracos@Logans-MacBook.local>
2022-05-31 08:51:15 -0400

    Stuff

--- Pickup_Comps_Inner/React/my-app/src/footer_components/About.js
+++ Pickup_Comps_Inner/React/my-app/src/footer_components/About.js
@@ -1,5 +1,13 @@
+import '..//Header_Components//Headers.css';
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+      <div>
+        <h1>About</h1>
+
+        <p>This is a penis.</p>
+      </div>
+    );
   };
   
 export default Home;
